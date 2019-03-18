e33e60384cabb529ca87ad989ab833db9f6bc8f6
Fredrik Forsell <fredrik.forsell@lyse.net>
2019-03-18 01:37:22 +0100

    Sample file (not in use)

---
+++ NoteShare/NoteShare/TEditorHtmlView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TEditor;
+using TEditor.Abstractions;
+using Xamarin.Forms;
+
+namespace NoteShare
+{
+    class TEditorHtmlView : StackLayout
+    {
+        //create bindable property, html
+        public string Html { get; set; }
+
+        WebView _displayWebView;
+        public TEditorHtmlView()
+        {
+            this.Orientation = StackOrientation.Vertical;
+
+           // Page that has children with buttons
+            this.Children.Add(new Button
+            {
+                Text = "HTML Editor",
+                HeightRequest = 100,
+                Command = new Command(async (obj) =>
+                {
+                    await ShowTEditor();
+                })
+            });
+            _displayWebView = new WebView() { HeightRequest = 500 };
+            this.Children.Add(_displayWebView);
+
+        }
+
+        async Task ShowTEditor()
+        {
+            TEditorResponse response = await CrossTEditor.Current.ShowTEditor("<!-- This is an HTML comment --><p>This is a test of the <strong style=\"font-size:20px\">TEditor</strong> by <a title=\"XAM consulting\" href=\"http://www.xam-consulting.com\">XAM consulting</a></p>");
+            if (response.IsSave)
+            {
+                if (response.HTML != null)
+                {
+                    _displayWebView.Source = new HtmlWebViewSource() { Html = response.HTML };
+                }
+            }
+        }
+    }
+}
