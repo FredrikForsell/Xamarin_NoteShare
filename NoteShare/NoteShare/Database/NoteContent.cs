25467472a130b3b1ad488a906d62e5c2fd169dea
fredrifo <fredrik.forsell@lyse.net>
2019-03-22 03:08:28 +0100

    Created new database for individual note contents

---
+++ NoteShare/NoteShare/Database/NoteContent.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using System.ComponentModel;
+using SQLiteNetExtensions.Attributes;
+
+namespace NoteShare.Database
+{
+    [Table("NoteContent")]
+    public class NoteContent : INotifyPropertyChanged
+    {
+
+        #region NoteContentId
+
+        private int _noteContentId;
+        [PrimaryKey, AutoIncrement, Unique]
+        public int NoteContentId
+        {
+            get
+            {
+                return _noteContentId;
+            }
+            set
+            {
+                this._noteContentId = value;
+
+                OnPropertyChanged(nameof(NoteContentId));
+            }
+        }
+        #endregion
+
+        private int _noteId;
+
+        [ForeignKey(typeof(Note)), NotNull]
+        public int NoteId
+        {
+            get
+            {
+                return _noteId;
+            }
+            set
+            {
+                this._noteId = value;
+
+                OnPropertyChanged(nameof(NoteId));
+            }
+        }
+
+        #region NoteContent
+        private string _noteItem;
+        public string NoteItem
+        {
+            get
+            {
+                return _noteItem;
+            }
+            set
+            {
+                this._noteItem = value;
+
+                OnPropertyChanged(nameof(NoteItem));
+            }
+        }
+        #endregion
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ NoteShare/NoteShare/Database/NoteContentDataAccess.cs
@@ -0,0 +1,26 @@
+﻿using NoteShare.Interfaces;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NoteShare.Database
+{
+    public class NoteContentDataAccess
+    {
+        private SQLiteConnection database;
+        private static object collisionLock = new object();
+
+        public NoteContentDataAccess()
+        {
+
+
+        }
+
+        
+
+    }
+}
