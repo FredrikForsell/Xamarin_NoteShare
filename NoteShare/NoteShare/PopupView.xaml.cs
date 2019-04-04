11a017892947432e511745704d7e341ad0b0d5ce
fredrifo <fredrik.forsell@lyse.net>
2019-04-04 03:08:49 +0200

    Implemented popup

---
+++ NoteShare/NoteShare/PopupView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using NoteShare.Database;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NoteShare
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PopupView
+	{
+        int elementid;
+        public PopupView (int elementid)
+		{
+			InitializeComponent ();
+            this.elementid = elementid;
+        }
+        public PopupView()
+        {
+            InitializeComponent();
+        }
+
+        public async void btn_submitAsync(object sender, ClickedEventArgs e)
+        {
+            String taskName = entry_taskName.Text;
+            NoteContentDataAccess noteDB = new NoteContentDataAccess();
+
+            NoteContent tempNoteContent = new NoteContent
+            {
+                NoteId = elementid,
+                NoteItem = taskName
+            };
+
+            noteDB.AddNewNoteContent(tempNoteContent);
+
+            //Remove popup
+            await PopupNavigation.Instance.PopAsync(true);
+        }
+
+        protected override void OnDisappearing()
+        {
+            MessagingCenter.Send<App>((App)Application.Current, "OnSaveNote");
+            base.OnDisappearing();
+        }
+
+        private void Entry_taskName_Focused(object sender, FocusEventArgs e)
+        {
+            entry_taskName.Text = null;
+
+        }
+    }
+}
